a563c6ada16fe5c2bf52aba804fc400476d49b7e
svatosa24 <svatosa24@spstrutnov.cz>
2026-04-15 11:31:39 +0200

    Zatim jeste nefunkcni

---
+++ Skolni portal/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skolni_portal.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email je povinný")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Heslo je povinné")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Skolni portal/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Skolni_portal.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Jméno je povinné")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Příjmení je povinné")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email je povinný")]
+        [EmailAddress(ErrorMessage = "Neplatný formát emailu")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@spstrutnov\.cz$", ErrorMessage = "Musíš použít školní email končící na @spstrutnov.cz!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Heslo je povinné")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potvrdit heslo")]
+        [Compare("Password", ErrorMessage = "Hesla se neshodují.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
