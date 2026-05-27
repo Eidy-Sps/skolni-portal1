da63b1f2add0acd1378ae32f260d201778e0cda4
Tomáš Urban <urbant24@spstrutnov.cz>
2026-05-27 11:18:27 +0200

    oprava kodu

--- Skolni portal/Program.cs
+++ Skolni portal/Program.cs
@@ -5,13 +5,9 @@ namespace Skolni_portal
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
